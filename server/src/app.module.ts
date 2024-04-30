e0fb0f1801a5de945f1b0c50faf3ceef9289e98e
RomanCLV <roman.clavier.2001@gmail.com>
2024-04-30 18:14:48 +0200

    add all modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SizesModule } from './sizes/sizes.module';
+import { GendersModule } from './genders/genders.module';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [
+        // __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      synchronize: Boolean(process.env.DEBUG),
+    }),
+    UsersModule,
+    CitiesModule,
+    CategoriesModule,
+    SizesModule,
+    GendersModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

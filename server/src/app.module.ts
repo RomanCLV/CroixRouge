b92d279a31fd16ed65c58dbff04e27738867f8e6
RomanCLV <roman.clavier.2001@gmail.com>
2024-05-16 12:29:02 +0200

    update login bdd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -36,7 +36,7 @@ import { SuperAdminsModule } from './super-admins/super-admins.module';
             host: process.env.DATABASE_HOST,
             port: Number(process.env.DATABASE_PORT),
             username: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
+            password: process.env.DATABASE_PASSWORD || "",
             database: process.env.DATABASE_NAME,
             entities: [Category, Size, Gender, User, City, Product, ProductImages, Cart, CityAdmin, SuperAdmin],
             synchronize: false,

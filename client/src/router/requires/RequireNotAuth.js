a56fcd24214129f1fc8b391f052675ebce30c382
RomanCLV <roman.clavier.2001@gmail.com>
2024-05-23 18:04:45 +0200

    remove console log

--- client/src/router/requires/RequireNotAuth.js
+++ client/src/router/requires/RequireNotAuth.js
@@ -6,12 +6,9 @@ import { ROUTES } from "../routes";
 const RequireNotAuth = ( { children } ) => {
 
     const connected = useSelector(isConnected);
-    console.log("connected:", connected)
     if (connected) {
-        console.log("redirection")
         return <Navigate to={ROUTES.root} />
     }
-    console.log("children")
     return children;
 };
 
